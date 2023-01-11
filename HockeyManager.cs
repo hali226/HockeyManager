ad7dd05937544d80b27aa55c2a3788aab43dd6b2
David Halás <43602311+hali226@users.noreply.github.com>
2023-01-11 22:08:36 +0100

    Add files via upload

---
+++ HockeyManager.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HockeyManager_namespace
+{
+    public class HockeyManager
+    {
+        string name;
+        int age;
+        int goals, hits, wins;
+
+        //keďže nepoužijem databázu, budem si to ukladať len do Listov, po skončení programu budú premazané
+        List<Forward> ForwardList = new List<Forward>();
+        List<Defender> DefenderList = new List<Defender>();
+        List<Goalie> GoalieList = new List<Goalie>();
+
+        public void AddNewForward(string name, int age, int goals)
+        {
+            this.name = name;
+            this.age = age;
+            this.goals = goals;
+            ForwardList.Add(new Forward(this.name, this.age, this.goals));
+        }
+
+        public void AddNewDefender(string name, int age, int hits)
+        {
+            this.name = name;
+            this.age = age;
+            this.hits = hits;
+            DefenderList.Add(new Defender(this.name, this.age, this.hits));
+        }
+
+        public void AddNewGoalie(string name, int age, int wins)
+        {
+            this.name = name;
+            this.age = age;
+            this.wins = wins;
+            GoalieList.Add(new Goalie(this.name, this.age, this.wins));
+        }
+
+    public void PrintNameAndAgeOfTheYoungestGoalie()
+    {
+            int youngestGoalieAge = 0, iters = 0;
+            string youngestGoalieName = null;
+
+            foreach (var iList in GoalieList)
+            {
+                    if ((iList.age < youngestGoalieAge) || (iters == 0))
+                    {
+                        youngestGoalieAge = iList.age;
+                        youngestGoalieName = iList.name;
+                    }
+                iters++;
+            }
+            Console.WriteLine("The youngest goalie is " + youngestGoalieName + ". He is " + youngestGoalieAge + " years old.");
+        }
+}
+
+    public class Player //následne využijem dedičnosť
+    {
+        public string name;
+        public int age;
+    }
+    public class Forward: Player
+    {
+        int goals;
+        public Forward(string name, int age, int goals)
+        {
+            this.name = name;
+            this.age = age;
+            this.goals = goals;
+        }
+    }
+
+    public class Defender: Player
+    {
+        int hits;
+        public Defender(string name, int age, int hits)
+        {
+            this.name = name;
+            this.age = age;
+            this.hits = hits;
+        }
+    }
+
+    public class Goalie: Player
+    {
+        public int wins;
+        public Goalie(string name, int age, int wins)
+        {
+            this.name = name;
+            this.age = age;
+            this.wins = wins;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace HockeyManager_namespace
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {   
+            string name;
+            char position;
+            int age, goals, hits, wins,i;
+            var myHockeyManager = new HockeyManager();
+
+            for (i = 0; i <= 3; i++) // dám to do cyklu, aby sa dalo zistiť najmladšieho brankára, keďže nepoužívam databázu
+            {
+                Console.WriteLine("Enter the player's position [G/D/F]: ");
+                position = Console.ReadLine()[0];
+
+                    switch (position)
+                    {
+                        case 'G':
+                            Console.WriteLine("Enter the player's name: ");
+                            name = Console.ReadLine();
+                            Console.WriteLine("Enter the player's age: ");
+                            age = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter the player's wins: ");
+                            wins = int.Parse(Console.ReadLine());
+                            myHockeyManager.AddNewGoalie(name, age, wins);
+                            break;
+
+                        case 'D':
+                            Console.WriteLine("Enter the player's name: ");
+                            name = Console.ReadLine();
+                            Console.WriteLine("Enter the player's age: ");
+                            age = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter the player's hits: ");
+                            hits = int.Parse(Console.ReadLine());
+                            myHockeyManager.AddNewGoalie(name, age, hits);
+                            break;
+
+                        case 'F':
+                            Console.WriteLine("Enter the player's name: ");
+                            name = Console.ReadLine();
+                            Console.WriteLine("Enter the player's age: ");
+                            age = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter the player's goals: ");
+                            goals = int.Parse(Console.ReadLine());
+                            myHockeyManager.AddNewGoalie(name, age, goals);
+                            break;
+
+                        default:
+                            Console.WriteLine("You wrote wrong letter.");
+                            break;
+                    }
+            }
+            myHockeyManager.PrintNameAndAgeOfTheYoungestGoalie(); //testovanie danej metódy po zbehnutí všetkých cyklov
+        }
+    }
+}
